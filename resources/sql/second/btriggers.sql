dc6ce8b0b26a5d32821e09f637cf27e42fc2a3f8
mequam <blue9ja@gmail.com>
2023-10-09 14:06:11 -0400

    got the vip adding properly
    NOTE: the luxury ticket does not yet add
    correctly, so thats gonna b a thing moving forwards

--- resources/sql/second/btriggers.sql
+++ resources/sql/second/btriggers.sql
@@ -6,16 +6,11 @@ CREATE TRIGGER IF NOT EXISTS SCHEDULE_INSERT
 INSTEAD OF INSERT ON SCHEDULE_TABLE BEGIN 
 
 	-- cursed hack I found online for sql lite variables
-	--PRAGMA temp_store = 2;
-	--CREATE TEMP TABLE _VARS(
-	--	name TEXT PRIMARY KEY,
-	--	int_val INTEGER,
-	--	date_val DATE);
 
 
 	--get the flight that we want to insert into
 	--and store it as a variable
---	INSERT INTO TEMP(name,int_val,date_val) 
+--	INSERT INTO VARS(name,int_val,date_val) 
 --		
 --		CASE 
 --			WHEN 
@@ -47,15 +42,72 @@ INSTEAD OF INSERT ON SCHEDULE_TABLE BEGIN
 		SEAT_NUMBER
 	) VALUES(
 		new.PASSENGER_TUID,
-		new.FLIGHT_TUID,
+		CASE 
+			WHEN new.requested_section = 'V'
+			THEN 
+				--select the first available flight
+				--that we can board
+				--REGAURLESS of luxury count
+				(SELECT flight_tuid 
+					FROM ALL_POSSIBLE_FLIGHTS 
+					WHERE 
+						--we use vip count wanted
+						--because that serves
+						--as a total indicator for
+						--all vip passengers in the plane
+						--basically, this asks
+						--"can this plane fit at least one more
+						--vip?"
+						vip_count_wanted < max_luxury+max_vip 
+						AND 
+					
+						--we need to limit the depart time
+						--to ONLY times that are greater than
+						--or equal to the depart time of the 
+						--insert command
+						NOT 
+						(
+							DATE(flight_date) 
+								= DATE(new.flight_date)
+						AND 
+							depart_time < (
+								
+								SELECT depart_time 
+								FROM FLIGHT_TABLE 
+								WHERE tuid = new.flight_tuid
+							)
+						)
+					ORDER BY flight_date, depart_time 
+					LIMIT 1)
+			ELSE  --we are a luxury passenger
+				--select the first flight we can board
+				--based on luxury limit
+		
+				(SELECT flight_tuid 
+					FROM ALL_POSSIBLE_FLIGHTS 
+					WHERE luxury_count < max_luxury 
+						AND 
+						NOT (
+							
+							DATE(flight_date) 
+								= DATE(new.flight_date)
+						AND 
+							depart_time < (
+								
+								SELECT depart_time 
+								FROM FLIGHT_TABLE 
+								WHERE tuid = new.flight_tuid
+							)
+						)
+					ORDER BY flight_date, depart_time 
+					LIMIT 1)
+		END,
 		new.FLIGHT_DATE,
 		new.REQUESTED_SECTION,
 		new.SEATED_SECTION,
 		new.SEAT_NUMBER
 	);
 
-	--remove the variables
-	--DROP TABLE _VARS;
 END;
 
 --CREATE TRIGGER IF NOT EXISTS AFTER_SCHEDULE_ADD 
